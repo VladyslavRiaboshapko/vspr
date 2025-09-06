c0341e143e6f1b623484736a2819adf3022fdd49
VladyslavRiaboshapko <krentrow@gmail.com>
2025-09-06 15:55:53 +0300

    secondCommit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace myApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = a + 10 / 5;
+            Console.WriteLine(b);
+        }
+    }
+}
